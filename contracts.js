951dbd9cf16e7b4278ab5b2f3f7ffa6e1586725d
daniil <tere00000000@gmail.com>
2022-11-26 18:02:51 +0300

    init repo

---
+++ contracts.js
@@ -0,0 +1,84 @@
+const Candidate = {
+    proffession: {
+        id: 1,
+        name: 'Программист'
+    },
+    personalQualities: [
+        {
+
+        },
+        {
+
+        },
+    ],
+    professionalQualities: [
+        {
+            id: 1,
+            name: 'Базы данных',
+        },
+        {
+            id: 2,
+            name: 'Английский язык'
+        },
+    ],
+    skills: [
+        {
+
+        },
+        {
+
+        },
+    ]
+}
+
+const filtersCandidate = {
+    industriy: {
+        id: 1,
+        name: 'it-технологии',
+        skills: [
+            {
+
+            },
+            {
+
+            },
+
+        ],
+        professionalQualities: [
+            {
+                id: 1,
+                name: 'Базы данных',
+            },
+            {
+
+            }
+        ],
+        personalQualities: [
+            {
+
+            },
+            {
+
+            },
+        ]
+    },
+    personalQualities: [
+        {
+
+        },
+        {
+
+        },
+    ]
+}
+
+const industries = [
+    {
+        id: 1,
+        name: 'it-технологии'
+    },
+    {
+        id: 2,
+        name: 'Финансы'
+    }
+]
